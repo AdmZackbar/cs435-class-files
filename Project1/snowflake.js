cf1a9b343415a79952045f73b57eff0b8d71e22b
Zach Wassynger <zach.wassynger@gmail.com>
2019-01-21 10:40:22 -0600

    Added initial files for hw1

---
+++ Project1/snowflake.js
@@ -0,0 +1,97 @@
+/*
+ * File: snowflake.js
+ * Author: Zach Wassynger
+ * Class: CS 435
+ * Purpose: Part of Project #1. Contains the program for generating a Koch snowflake.
+ */
+
+var canvas;
+var gl;
+
+var points = []; // Holds all the points defining the snowflake
+
+var trianglePoint1 = vec2(-0.5, -0.5); // Defines the verticies of the starting triangle
+var trianglePoint2 = vec2(0, 0.5);
+var trianglePoint3 = vec2(0.5, -0.5);
+
+var numIter = 5; // Defines the number of times to recurse through the algorithm
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    // Initialize the vertices array with the defined starting points
+    
+    var vertices = [
+        trianglePoint1,
+        trianglePoint2,
+        trianglePoint3
+    ];
+
+    (vertices[0], vertices[1], vertices[2], numIter);
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+function triangle( a, b, c )
+{
+    points.push( a, b, c );
+}
+
+function divideTriangle( a, b, c, count )
+{
+
+    // check for end of recursion
+    
+    if ( count === 0 ) {
+        triangle( a, b, c );
+    }
+    else {
+    
+        //bisect the sides
+        
+        var ab = mix( a, b, 0.5 );
+        var ac = mix( a, c, 0.5 );
+        var bc = mix( b, c, 0.5 );
+
+        --count;
+
+        // three new triangles
+        
+        divideTriangle( a, ab, ac, count );
+        divideTriangle( c, ac, bc, count );
+        divideTriangle( b, bc, ab, count );
+    }
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, points.length );
+}
