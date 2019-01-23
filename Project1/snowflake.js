fe861ecd845fcc35568b213f44e218c86a33f6d0
Zacharighteous <zawassynger@crimson.ua.edu>
2019-01-22 18:15:21 -0600

    Cleaned up files to improve readability

--- Project1/snowflake.js
+++ Project1/snowflake.js
@@ -8,15 +8,18 @@
 var canvas;
 var gl;
 
-var points = []; // Holds all the points defining the snowflake
+// Holds all the points defining the snowflake
+var points = [];
 
+// Defines the vertices of the starting triangle
 var vertices = [
-    vec2(-0.9, -0.45), // Defines the verticies of the starting triangle
+    vec2(-0.9, -0.45),
     vec2(0, 1),
     vec2(0.9, -0.45)
 ];
 
-var numIter = 5; // Defines the number of times to recurse through the algorithm
+// Defines the number of times to recurse through the algorithm
+var numIter = 5;
 
 window.onload = function init()
 {
@@ -25,6 +28,7 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
+    // Create the edges in a counter-clockwise direction
     var edges = [
         [vertices[2], vertices[1]],
         [vertices[1], vertices[0]],
@@ -36,25 +40,20 @@ window.onload = function init()
         generateTriangle(edges[i][0], edges[i][1], numIter);
     }
     
-    //
     //  Configure WebGL
-    //
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
     //  Load shaders and initialize attribute buffers
-    
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
     // Load the data into the GPU
-    
     var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
     // Associate out shader variables with our data buffer
-    
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
@@ -62,27 +61,27 @@ window.onload = function init()
     render();
 };
 
-function generateTriangle(A, B, iterLeft)
+function generateTriangle(vertexOne, vertexTwo, iterLeft)
 {
-    if (iterLeft === 0)
+    if (iterLeft === 0) // Base case: add the given points to the array
     {
-        points.push(A, B);
+        points.push(vertexOne, vertexTwo);
     }
     else
     {
-        var C = divide(add(multiply(A, 2), B), 3);
-        var D = divide(add(multiply(B, 2), A), 3);
-        var F = divide(add(A, B), 2);
+        var newVertexOne = divide(add(multiply(vertexOne, 2), vertexTwo), 3);
+        var newVertexThree = divide(add(multiply(vertexTwo, 2), vertexOne), 3);
+        var midpoint = divide(add(vertexOne, vertexTwo), 2);
         
-        var V1 = divide(minus(F, A), length(F, A));
+        var V1 = divide(minus(midpoint, vertexOne), length(midpoint, vertexOne));
         var V2 = [V1[1], -V1[0]];
 
-        var E = add(multiply(V2, Math.sqrt(3)/6 * length(B, A)), F);
+        var newVertexTwo = add(multiply(V2, Math.sqrt(3)/6 * length(vertexTwo, vertexOne)), midpoint);
 
-        generateTriangle(A, C, iterLeft-1);
-        generateTriangle(C, E, iterLeft-1);
-        generateTriangle(E, D, iterLeft-1);
-        generateTriangle(D, B, iterLeft-1);
+        generateTriangle(vertexOne, newVertexOne, iterLeft-1);
+        generateTriangle(newVertexOne, newVertexTwo, iterLeft-1);
+        generateTriangle(newVertexTwo, newVertexThree, iterLeft-1);
+        generateTriangle(newVertexThree, vertexTwo, iterLeft-1);
     }
 }
 
