0f38543d5d16dc49d67b0563adde55aeeaaec88f
Zach Wassynger <zach.wassynger@gmail.com>
2019-03-20 15:06:52 -0500

    Added initial vertex shaded work

--- Project4/shadedSphere2.js
+++ Project4/shadedSphere2.js
@@ -27,15 +27,19 @@ var modelViewMatrixLoc, projectionMatrixLoc;
 
 var normalMatrix, normalMatrixLoc;
 
+var ambientProductLoc, diffuseProductLoc, specularProductLoc;
+var lightPositionLoc, shininessLoc, lightAngleLoc;
+
 var lightDirectionLoc;
 
 var eye = vec3(0.0, 0.0, 1.0);
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
-
 var NUM_POS = 9;
 var spotlightX = Math.floor(NUM_POS/2), spotlightY = Math.floor(NUM_POS/2);
+
+var numSubdivisions = 1;
 
 var useVertexShading = false;
 
@@ -91,40 +95,15 @@ window.onload = function init() {
     gl.viewport(0, 0, canvas.width, canvas.height);
     
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
-    gl.enable(gl.DEPTH_TEST); 
+    gl.enable(gl.DEPTH_TEST);
     
-    // Load shaders and initialize attribute buffers
-    var program = initShaders(gl, "vertex-shader-f", "fragment-shader-f");
-    
-    gl.useProgram(program);
-
     createStage();
-
-    // Create and initialize buffer objects
-    var nBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
     
-    var vNormal = gl.getAttribLocation( program, "vNormal" );
-    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vNormal);
-
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-    
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-    normalMatrixLoc = gl.getUniformLocation( program, "worldInvTrans" );
-    lightDirectionLoc = gl.getUniformLocation(program, "lightDirection");
-
     ambientProduct = mult(lightAmbient, materialAmbient);
     diffuseProduct = mult(lightDiffuse, materialDiffuse);
     specularProduct = mult(lightSpecular, materialSpecular);
+    
+    onFragment();
 
     document.getElementById("ButtonUp").onclick = onUp;
     document.getElementById("ButtonDown").onclick = onDown;
@@ -132,16 +111,11 @@ window.onload = function init() {
     document.getElementById("ButtonRight").onclick = onRight;
     document.getElementById("ButtonVertex").onclick = onVertex;
     document.getElementById("ButtonFrag").onclick = onFragment;
-
-    gl.uniform4fv(gl.getUniformLocation(program, "ambientProduct"), flatten(ambientProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "diffuseProduct"), flatten(diffuseProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "specularProduct"), flatten(specularProduct));
-    gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));
-    gl.uniform1f(gl.getUniformLocation(program, "shininess"), materialShininess);
-    gl.uniform1f(gl.getUniformLocation(program, "lightAngle"), lightAngle);
-
-    projectionMatrix = ortho(-3, 3, -3, 3, -10, 10);
-    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+    
+    document.getElementById("SliderSubdivision").onchange = function() {
+        numSubdivisions = event.srcElement.value;
+        render();
+    }
 
     render();
 }
@@ -185,13 +159,68 @@ function onRight()
 function onVertex()
 {
     useVertexShading = true;
-    gl.useProgram(initShaders(gl, "vertex-shader-v", "fragment-shader-v"));
+    // Load shaders and initialize attribute buffers
+    var program = initShaders(gl, "vertex-shader-v", "fragment-shader-v");
+    
+    gl.useProgram(program);
+
+    initBufferObjects(program);
+    
+    render();
 }
 
 function onFragment()
 {
     useVertexShading = false;
-    gl.useProgram(initShaders(gl, "vertex-shader-f", "fragment-shader-f"));
+    // Load shaders and initialize attribute buffers
+    var program = initShaders(gl, "vertex-shader-f", "fragment-shader-f");
+    
+    gl.useProgram(program);
+
+    initBufferObjects(program);
+    
+    render();
+}
+
+function initBufferObjects(program)
+{
+    // Create and initialize buffer objects
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+    
+    var vNormal = gl.getAttribLocation( program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+    
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+    normalMatrixLoc = gl.getUniformLocation(program, "worldInvTrans");
+    lightDirectionLoc = gl.getUniformLocation(program, "lightDirection");
+    ambientProductLoc = gl.getUniformLocation(program, "ambientProduct");
+    diffuseProductLoc = gl.getUniformLocation(program, "diffuseProduct");
+    specularProductLoc = gl.getUniformLocation(program, "specularProduct");
+    lightPositionLoc = gl.getUniformLocation(program, "lightPosition");
+    shininessLoc = gl.getUniformLocation(program, "shininess");
+    lightAngleLoc = gl.getUniformLocation(program, "lightAngle");
+    
+    gl.uniform4fv(ambientProductLoc, flatten(ambientProduct));
+    gl.uniform4fv(diffuseProductLoc, flatten(diffuseProduct));
+    gl.uniform4fv(specularProductLoc, flatten(specularProduct));
+    gl.uniform4fv(lightPositionLoc, flatten(lightPosition));
+    gl.uniform1f(shininessLoc, materialShininess);
+    gl.uniform1f(lightAngleLoc, lightAngle);
+    
+    projectionMatrix = ortho(-3, 3, -3, 3, -10, 10);
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
 }
 
 function render() {
@@ -199,33 +228,65 @@ function render() {
     
     //eye = vec3(radius*Math.sin(theta)*Math.cos(phi), 
     //    radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
-
-    lightDirection = vec3(subtract(vec4(spotlightX-Math.floor(NUM_POS/2), spotlightY-Math.floor(NUM_POS/2), 0, 0), lightPosition));
-    //console.log(lightDirection);
-    gl.uniform3fv(lightDirectionLoc, flatten(lightDirection));
-
-    modelViewMatrix = lookAt(eye, at, up);
-    modelViewMatrix = mult(modelViewMatrix, scale4(3, 3, 1));
-    //modelViewMatrix = mult(modelViewMatrix, translate(0, 3/2, 0));
-    modelViewMatrix = mult(modelViewMatrix, rotate(delta, 0, 1, 0));
-    //modelViewMatrix = mult(modelViewMatrix, rotate(delta2, 1, 0, 0));
-    //projectionMatrix = ortho(left, right, bottom, ytop, near, far);
     
-    normalMatrix = [
-        vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
-        vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
-        vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
-    ];
-    normalMatrix = transpose(inverse3(normalMatrix));
-
-            
-    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    //gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
-    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix) );
+    lightDirection = vec3(subtract(vec4(spotlightX-Math.floor(NUM_POS/2), spotlightY-Math.floor(NUM_POS/2), 0, 0), lightPosition));
+    gl.uniform3fv(lightDirectionLoc, flatten(lightDirection));
+    
+    if (useVertexShading)
+    {
+        var width = 6/numSubdivisions;
+        var offset = -3 + (width/2);
+        var templateMatrix = lookAt(eye, at, up);
+        var rowMatrix = mult(templateMatrix, translate(offset, 0, 0));
+        for (var i = 0; i < numSubdivisions; i++)
+        {
+            var columnMatrix = mult(rowMatrix, translate(0, offset, 0));
+            for (var j = 0; j < numSubdivisions; j++)
+            {
+                modelViewMatrix = mult(columnMatrix, scale4(width/2, width/2, 1));
+                
+                normalMatrix = [
+                    vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+                    vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+                    vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+                ];
+                normalMatrix = transpose(inverse3(normalMatrix));
+                
+                gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+                gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix));
+                
+                gl.drawArrays(gl.TRIANGLES, 0, 6);
+                
+                columnMatrix = mult(columnMatrix, translate(0, width, 0));
+            }
+            rowMatrix = mult(rowMatrix, translate(width, 0, 0))
+        }
+    }
+    else
+    {
+        modelViewMatrix = lookAt(eye, at, up);
+        modelViewMatrix = mult(modelViewMatrix, scale4(3, 3, 1));
+        //modelViewMatrix = mult(modelViewMatrix, translate(0, 3/2, 0));
+        modelViewMatrix = mult(modelViewMatrix, rotate(delta, 0, 1, 0));
+        //modelViewMatrix = mult(modelViewMatrix, rotate(delta2, 1, 0, 0));
+        //projectionMatrix = ortho(left, right, bottom, ytop, near, far);
         
-    //for( var i=0; i<index; i+=3) 
-    //    gl.drawArrays( gl.TRIANGLES, i, 3 );
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+        normalMatrix = [
+            vec3(modelViewMatrix[0][0], modelViewMatrix[0][1], modelViewMatrix[0][2]),
+            vec3(modelViewMatrix[1][0], modelViewMatrix[1][1], modelViewMatrix[1][2]),
+            vec3(modelViewMatrix[2][0], modelViewMatrix[2][1], modelViewMatrix[2][2])
+        ];
+        normalMatrix = transpose(inverse3(normalMatrix));
 
-    //window.requestAnimFrame(render);
+                
+        gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+        //gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
+        gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMatrix));
+            
+        //for( var i=0; i<index; i+=3) 
+        //    gl.drawArrays( gl.TRIANGLES, i, 3 );
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+        //window.requestAnimFrame(render);
+    }
 }
